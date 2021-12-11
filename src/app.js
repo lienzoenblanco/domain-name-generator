b06f3fa22208e3252f8602a4099fc58fb9f39246
lienzoenblanco <raquelgarciatrives@gmail.com>
2021-12-11 13:10:41 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our", "his", "my"];
+let adj = ["great", "big", "green", "awesome"];
+let noun = ["jogger", "racoon", "cow", "duck"];
+let exten = [".com", ".es", ".org", ".net"];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //document.querySelector("#domain").innerHTML = generateDomain();
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < exten.length; l++) {
+          document.querySelector("#domain").innerHTML =
+            document.querySelector("#domain").innerHTML +
+            pronoun[i].concat(adj[j], noun[k], exten[l], "</br>");
+          //   document.write(
+          //     pronoun[i].concat(adj[j], noun[k], exten[l]) + "</br>"
+          //  );
+        }
+      }
+    }
+  }
 };
